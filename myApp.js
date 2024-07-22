e7622d755d1e857848c49d74e04bacebe5967558
Piyush <vaidpiyush.30@gmail.com>
2024-07-22 21:34:47 +0530

    Delete Many Documents with model.remove()

--- myApp.js
+++ myApp.js
@@ -146,7 +146,11 @@ const removeById = (personId, done) => {
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
 
-  done(null /*, data*/);
+  Person.remove({ name: nameToRemove }, (err, outCome) => {
+    if (err) return console.error(err);
+
+    done(null, outCome);
+  });
 };
 
 const queryChain = (done) => {
